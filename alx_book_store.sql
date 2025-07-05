4a41aee0ed51118b74bda9f084e7ac8321a72db6
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-05 16:13:09 +0300

    sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -13,7 +13,7 @@ CREATE TABLE Books (
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-CREATE TABLE Customer (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(130) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
