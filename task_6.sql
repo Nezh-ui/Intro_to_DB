630499e3a5b7b0d4a366478aca407cbb423677bd
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-07 07:02:55 +0300

    33

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
-INSERT INTO Customers("customer_id","customer_name","email","address") VALUES
+INSERT INTO Customers ("customer_id","customer_name","email","address") VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness AVE.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness AVE.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness AVE.'),
